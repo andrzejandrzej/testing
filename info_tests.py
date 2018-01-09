1d2854beb7277fa23879bb1e020facba0ee47d1e
Andrzej Dębicki <andrzej.debicki@nask.pl>
2018-01-09 14:18:00 +0100

    Info resource tests.

---
+++ info_tests.py
@@ -0,0 +1,19 @@
+import mock
+
+
+class Foo(object):
+
+    registry = mock.Mock()
+    registry.auth_api = mock.Mock()
+
+
+    def __init__(self):
+        self.registry.auth_api.get_access_info.return_value = dict(res_limits=['a', 'b'], full='true')
+        self.registry.full_access = False
+
+
+f = Foo()
+print f.registry.auth_api.get_access_info({'a': 123, 'b': 3426})
+print f.registry.full_access
+print getattr(f.registry, 'auth_api')
+
